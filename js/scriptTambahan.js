23e0a0f2527d66b8857958e3ad0a6503c9918c08
Muhammad Arya Java <muhammad.aryajava@gmail.com>
2022-04-03 01:27:19 +0700

    Menambahkan include tag

--- js/scriptTambahan.js
+++ js/scriptTambahan.js
@@ -1,3 +1,18 @@
+// Include Tag
+document.addEventListener("DOMContentLoaded", function(){
+  let includes = document.getElementsByTagName('include');
+  for(var i=0; i<includes.length; i++){
+      let include = includes[i];
+      load_file(includes[i].attributes.src.value, function(text){
+          include.insertAdjacentHTML('afterend', text);
+          include.remove();
+      });
+  }
+  function load_file(filename, callback) {
+      fetch(filename).then(response => response.text()).then(text => callback(text));
+  }
+});
+
 //Get the button
 let mybutton = document.getElementById("btn-back-to-top");
 
